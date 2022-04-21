969afdafe8600ab4afd909df88370004fb6de3d9
Woska <60791044+agwoska@users.noreply.github.com>
2022-04-21 16:29:12 -0400

    accelerometer added

--- src/CSE499_main.cpp
+++ src/CSE499_main.cpp
@@ -2,13 +2,18 @@
  * @file main.cpp
  * @date 2022-04-07
  * @author Andrew W, Imani M-G, Daniel M
- * last updated 2022-04-14
+ * @brief CSE 499 self balancing robot project implementation
+ * last updated 2022-04-21
  */
 
 #include "mbed.h"
 #include "CSE499_main.h"
+#include "MMA8452.h"
 
-// fix pins later
+/* instantiate objects and variables */
+
+MMA8452 accel(PB_9, PB_8, 100000);
+
 DigitalIn tiltHF(PD_14);
 DigitalIn tiltSF(PD_15);
 DigitalIn tiltSB(PD_12);
@@ -18,22 +23,59 @@ DigitalOut tiltF(PF_12);
 DigitalOut tiltB(PD_9);
 
 DigitalOut m1(PF_13);
-DigitalOut m2(PE_9);
-DigitalOut m3(PE_11);
+DigitalOut ml1(PE_9);
+DigitalOut ml2(PE_11);
+DigitalOut mr1(PF_14);
+DigitalOut mr2(PE_13);
 
+uint8_t tilt;
+
+/* implementation */
 
 int main() {
-    tiltF = 1;
-    tiltB = 1;
-    m1 = 1;
-    m2 = 1;
-    m3 = 0;
-    uint8_t tilt = BALANCED;
+    // setup variables to hold important data
+    double accel_data[3] = { 0 };
+    double roll = 0.0;
+
+    setup();
+    // tiltF = 1;
+    // tiltB = 1;
     while (true) {
+        // tilt = checkTilt();
+        ml1 = !ml1;
+        ml2 = !ml2;
+        mr1 = !mr1;
+        mr2 = !mr2;
+        // check if I2C ready
+        if ( !accel.isXYZReady() ) {
+            ThisThread::sleep_for(2ms);
+        }
+        // get accelerometer output
+        accel.readXYZGravity(&accel_data[0], &accel_data[1], &accel_data[2]);
+        // get tilt angle
+        roll = calcRoll(accel_data[1], accel_data[2]);
+        printf("Grav: %3.2f\t%3.2f\t%3.2f\t%3.2f\n\r", accel_data[0], accel_data[1], accel_data[2], roll);
+        // printf("%d\n\r", tilt);
         ThisThread::sleep_for(1s);
-        tilt = checkTilt();
-        printf("%d\n\r", tilt);
     }
+}
+
+
+void setup() {
+    accel.setBitDepth(MMA8452::BIT_DEPTH_12);
+    accel.setDynamicRange(MMA8452::DYNAMIC_RANGE_2G);
+    accel.setDataRate(MMA8452::RATE_100);
+    m1  = 1;
+    ml1 = 1;
+    ml2 = 0;
+    mr1 = 0;
+    mr2 = 1;
+    tilt = BALANCED;
+}
+
+
+double calcRoll(double y, double z) {
+    return atan2(y,z) * RAD_TO_DEG;
 }
 
 
@@ -52,3 +94,4 @@ tilt_pos_t checkTilt() {
     }
     return BALANCED;
 }
+
--- src/CSE499_main.h
+++ src/CSE499_main.h
@@ -2,9 +2,19 @@
  * @file main.cpp
  * @date 2022-04-14
  * @author Andrew W, Imani M-G, Daniel M
- * last updated 2022-04-14
+ * @brief CSE 499 self balancing robot project header
+ * last updated 2022-04-21
  */
 
+ #pragma once
+
+ #ifndef CSE499_MAIN_H
+ #define CSE499_MAIN_H
+
+/** constants **/
+
+#define PI          ( 3.14159 )
+#define RAD_TO_DEG  ( 180.0 / (PI) )
 
 /** structure **/
 
@@ -17,10 +27,25 @@ typedef enum {
 } tilt_pos_t;
 
 
- /** prototype function **/
+/** prototype function **/
+
+/**
+ * @brief sets up sensors
+ */
+void setup();
+
+/**
+ * @brief calculates the roll of the device
+ * @param y the y-axis output from the accelerometer
+ * @param z the z-axis output from the accelerometer
+ * @return the calculated roll value 
+ */
+double calcRoll(double y, double z);
 
 /**
  * checks the tilt angle
  * @return tilt level
  */
  tilt_pos_t checkTilt();
+
+#endif // CSE499_MAIN_H
